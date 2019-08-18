3c15884c1645f8c5661b0cf7a8ab1c9ff32e5a74
Eugene Worker <1zheka92@gmail.com>
2019-08-18 16:51:33 +0300

    Updates

---
+++ precache-manifest.f55f2ff95af6bdca5dd0ad3e2f35c348.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "002177a95c7cf8b257449a32a5718de5",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "758ece12b2e5324a937d",
+    "url": "/todo-app/static/css/main.ae69dd1d.chunk.css"
+  },
+  {
+    "revision": "3c2222298d732a17d7c9",
+    "url": "/todo-app/static/js/2.77060bd4.chunk.js"
+  },
+  {
+    "revision": "758ece12b2e5324a937d",
+    "url": "/todo-app/static/js/main.c5a81291.chunk.js"
+  },
+  {
+    "revision": "f7728c30f6339eef41ed",
+    "url": "/todo-app/static/js/runtime~main.eca9ff85.js"
+  },
+  {
+    "revision": "514e30d9cc65400fed47c8af4489a9a1",
+    "url": "/todo-app/static/media/NunitoSans-Regular.514e30d9.woff"
+  },
+  {
+    "revision": "990fe215c5627c98e8591be452a30c12",
+    "url": "/todo-app/static/media/NunitoSans-Regular.990fe215.woff2"
+  }
+]);
